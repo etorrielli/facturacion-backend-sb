04e2c0d328fa75d635f1ceee8ef0271bab7660c8
edgar <e.torrielli@gmail.com>
2019-09-11 21:01:04 -0300

    first

---
+++ src/main/java/com/undec/facturacion/controller/FacturaController.java
@@ -0,0 +1,44 @@
+package com.undec.facturacion.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.undec.facturacion.dto.FacturaDTO;
+import com.undec.facturacion.dto.Response;
+import com.undec.facturacion.model.Factura;
+import com.undec.facturacion.service.FacturaService;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/facturas")
+public class FacturaController {
+
+	@Autowired
+	private FacturaService facturaService;
+
+	@PostMapping
+    public ResponseEntity<?> save(@RequestBody FacturaDTO facturaDTO) throws Exception {
+        Response response = facturaService.save(facturaDTO);
+        return ResponseEntity.ok().body(response);
+    }
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Response> get(@PathVariable("id") int id) throws Exception {
+		Response response = facturaService.findOneById(String.valueOf(id));
+		return ResponseEntity.ok().body(response);
+	}
+
+	@GetMapping()
+	public ResponseEntity<?> list() throws Exception {
+		Response response = facturaService.findAll();
+		return ResponseEntity.ok().body(response);
+	}
+
+}
---
+++ src/main/java/com/undec/facturacion/service/FacturaService.java
@@ -0,0 +1,121 @@
+package com.undec.facturacion.service;
+
+import com.undec.facturacion.dto.DetalleDTO;
+import com.undec.facturacion.dto.FacturaDTO;
+import com.undec.facturacion.model.Detalle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+import com.undec.facturacion.dto.Response;
+import com.undec.facturacion.model.Factura;
+import com.undec.facturacion.repository.*;
+
+
+/**
+ * @author etorrielli
+ */
+@Service
+public class FacturaService {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(FacturaService.class);
+
+    @Autowired
+    private FacturaRepository facturaRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Autowired
+    private DetalleRepository detalleRepository;
+
+    public Response findAll() throws Exception {
+        Response response = new Response();
+        try {
+            List<Factura> facturaList = facturaRepository.findAll();
+            response.setData(facturaList);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return response;
+    }
+
+    public Response findOneById(String id) throws Exception {
+        Response response = new Response();
+        try {
+            Factura factura = facturaRepository.findById(Integer.parseInt(id)).get();
+            response.setData(factura);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return response;
+    }
+
+    public Response update(FacturaDTO input) throws Exception {
+        Response response = new Response();
+        try {
+            Factura factura = facturaRepository.findById(input.getId()).get();
+            facturaRepository.save(factura);
+
+            response.setData(input);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return response;
+    }
+
+    public Response save(FacturaDTO facturaDTO) throws Exception {
+        Response response = new Response();
+        try {
+            Factura factura = facturaDTOToEntity(facturaDTO);
+            factura = facturaRepository.save(factura);
+
+            for (DetalleDTO item : facturaDTO.getDetalles()) {
+                Detalle detalle = detalleDTOToEntity(factura, item);
+                detalle = detalleRepository.save(detalle);
+            }
+
+            response.setData(factura.getId());
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return response;
+    }
+
+    private Factura facturaDTOToEntity(FacturaDTO facturaDTO) throws Exception {
+        Factura factura = new Factura();
+        factura.setFolio(facturaDTO.getFolio());
+        factura.setDescripcion(facturaDTO.getDescripcion());
+        factura.setObservacion(facturaDTO.getObservacion());
+        factura.setFecha(new Date());
+        factura.setClienteByClienteId(clienteRepository.getOne(facturaDTO.getClienteId()));
+        return factura;
+    }
+
+    private Detalle detalleDTOToEntity(Factura factura, DetalleDTO detalleDTO) throws Exception {
+        Detalle detalle = new Detalle();
+        detalle.setCantidad(detalleDTO.getCantidad());
+        detalle.setFacturaByFacturaId(factura);
+        detalle.setProductoByProductoId(productoRepository.getOne(detalleDTO.getProductoId()));
+        return detalle;
+    }
+
+}
